f1a5aaa83bf438ebe066696cbe58bca0cacaaea4
Rabii Kahlaoui <rabii.kahlaouiii@gmail.com>
2022-06-05 21:33:47 +0100

    Run smart contract functions

--- client/src/pages/Rentals.js
+++ client/src/pages/Rentals.js
@@ -3,7 +3,7 @@ import "./Rentals.css";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
 import logo from "../images/airbnbRed.png";
-import { ConnectButton, Icon, Button } from "web3uikit";
+import { ConnectButton, Icon, Button, useNotification } from "web3uikit";
 import RentalsMap from "../components/RentalsMap";
 import { useState, useEffect } from "react";
 import { useMoralis, useWeb3ExecuteFunction } from "react-moralis";
@@ -11,9 +11,38 @@ import { useMoralis, useWeb3ExecuteFunction } from "react-moralis";
 const Rentals = () => {
   const { state: searchFilters } = useLocation();
   const [ highLight, setHighLight ] = useState();
-  const { Moralis } = useMoralis();
+  const { Moralis, account } = useMoralis();
   const [ rentalsList, setRentalsList ] = useState();
   const [ coordinates, setCoordinates ] = useState([]);
+  const contractProcessor = useWeb3ExecuteFunction();
+  const dispatch = useNotification();
+
+  const handleSuccess = () => {
+    dispatch({
+      type: "success",
+      message: `Nice, You're going to ${searchFilters.destination}`,
+      title: "Booking Succesful",
+      position: "topL",
+    });
+  }
+
+  const handleError = (msg) => {
+    dispatch({
+      type: "error",
+      message: `${msg}`,
+      title: "Booking failed",
+      position: "topL",
+    });
+  }
+
+  const handleNoAccount = () => {
+    dispatch({
+      type: "error",
+      message: "You need to connect your wallet to book rental",
+      title: "Not Connected",
+      position: "topL",
+    });
+  }
 
   useEffect(() => {
 
@@ -39,6 +68,59 @@ const Rentals = () => {
     fetchRentalsList();
 
   }, [searchFilters]);
+
+  const bookRental = async function (start, end, id, dayPrice) {
+
+    for (
+      var arr = [], dt = new Date(start);
+      dt < end;
+      dt.setDate(dt.getDate() + 1)
+    ) {
+      arr.push(new Date(dt).toISOString().slice(0, 10));
+    }
+
+    let options = {
+      contractAddress: "0xb49e4a464fa9f6E7c179a2834ff1fA76ac5a6B8D",
+      functionName: "addDatesBooked",
+      abi: [
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "id",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string[]",
+              "name": "newBookings",
+              "type": "string[]"
+            }
+          ],
+          "name": "addDatesBooked",
+          "outputs": [],
+          "stateMutability": "payable",
+          "type": "function"
+        }
+      ],
+      params: {
+        id: id,
+        newBookings: arr
+      },
+      msgValue: Moralis.Units.ETH(dayPrice * arr.length)
+    }
+
+    await contractProcessor.fetch({
+      params: options,
+      onSuccess: () => {
+        handleSuccess();
+      },
+      onError: (error) => {
+        console.log(error);
+        handleError(error.data.message);
+      }
+    });
+
+  }
 
   return (
     <>
@@ -90,7 +172,21 @@ const Rentals = () => {
                       <div className="rentalDesc">{e.attributes.unoDescription}</div>
                       <div className="rentalDesc">{e.attributes.dosDescription}</div>
                       <div className="bottomButton">
-                        <Button text="Stay here" />
+                        <Button
+                          onClick={() => {
+                            if (account) {
+                              bookRental(
+                                searchFilters.checkIn,
+                                searchFilters.checkOut,
+                                e.attributes.uid_decimal.value.$numberDecimal,
+                                Number(e.attributes.pricePerDay_decimal.value.$numberDecimal)
+                              );
+                            } else {
+                              handleNoAccount();
+                            }
+                          }}
+                          text="Stay here"
+                        />
                         <div className="price">
                           <Icon fill="#808080" size={10} svg="matic" /> {e.attributes.pricePerDay} / Day
                         </div>
